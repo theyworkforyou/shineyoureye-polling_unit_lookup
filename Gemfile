ef1f84ec0407ce9531ec86e0741ff47c2155fd20
Chris Mytton <chrismytton@gmail.com>
2016-02-25 15:23:47 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+ruby '2.3.0'
+
+eval_gemfile 'shineyoureye/Gemfile'
+
+source 'https://rubygems.org' do
+  gem 'sinatra'
+  gem 'rest-client'
+  gem 'sinatra-jekyll'
+  gem 'puma'
+  gem 'pry'
+end
---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'bundler'
+Bundler.require
+
+require 'json'
+
+configure do
+  set :jekyll_site_path, 'shineyoureye'
+  set :pu_lookup_api_url, 'https://pu-lookup.herokuapp.com'
+end
+
+helpers do
+  def pu_lookup(pun)
+    response = RestClient.get(
+      settings.pu_lookup_api_url,
+      params: { lookup: pun }
+    )
+    JSON.parse(response, symbolize_names: true)
+  rescue RestClient::ResourceNotFound => e
+    logger.warn(e.message)
+    nil
+  end
+end
+
+get '/' do
+  if params[:q]
+    @area = pu_lookup(params[:q])
+  end
+  render_into_jekyll_layout erb(:index), 'disable_breadcrumbs' => true
+end
